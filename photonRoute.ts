226b7de41b7cab85579dfe10157951fd48b7f049
Mael <laem@kont.me>
2025-05-09 13:01:44 +0200

    Téléchargement optionnel des données photon

--- photonRoute.ts
+++ photonRoute.ts
@@ -2,8 +2,8 @@ import { download, liveExec } from './tiles.js'
 const secretKey = process.env.SECRET_KEY
 
 export default function photonRoute(app) {
-  app.get('/update-photon/:givenSecretKey', async (req, res) => {
-    const { givenSecretKey } = req.params
+  app.get('/update-photon/:givenSecretKey/:noDownload', async (req, res) => {
+    const { givenSecretKey, noDownload = false } = req.params
     if (givenSecretKey !== secretKey) {
       return res
         .status(401)
@@ -18,14 +18,13 @@ export default function photonRoute(app) {
       'cd ~ && wget -O -  | pbzip2 -cd | tar x'
     )
 	*/
-      const url = `https://download1.graphhopper.com/public/photon-db-latest.tar.bz2`
-      await download(url)
 
-      /*
-      await liveExec(
-        'mv photon-db-latest.tar.bz2 ~/ && cd ~ / && pbzip2 -cd photon-db-latest.tar.bz2 | tar x'
-      )
-	  */
+      if (!noDownload) {
+        const url = `https://download1.graphhopper.com/public/photon-db-latest.tar.bz2`
+        await download(url)
+      }
+
+      await liveExec('pbzip2 -cdv photon-db-latest.tar.bz2 | tar x')
 
       console.log('-------------------------------')
       console.log('✅ Downloaded photon database 🌍️')
